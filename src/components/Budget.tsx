b7a1dffe26a86a57c065e488135b7a7c8f4d4b88
douarmoua <mouadouarzong@gmail.com>
2023-12-28 21:41:44 -0600

    clean up + add redux

--- src/components/Budget.tsx
+++ src/components/Budget.tsx
@@ -1,13 +1,8 @@
-import PlanBudget from "./PlanBudget";
-import {Route, Routes} from 'react-router-dom';
 
 function Budget() {
     return (
         <>
             <div>Budget Page</div>
-            {/*<Routes>*/}
-            {/*    <Route path="expenses" element={<PlanBudget/>}/>*/}
-            {/*</Routes>*/}
         </>
     )
 }
