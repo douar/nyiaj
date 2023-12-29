0cb132b04486931c2969bc840c9a9ceae4ec320d
douarmoua <mouadouarzong@gmail.com>
2023-12-28 21:01:41 -0600

    Add init nav and home

---
+++ src/components/home.tsx
@@ -0,0 +1,11 @@
+function Home() {
+
+    return (
+        <div>
+            Log in to Your Account
+        </div>
+    )
+
+}
+
+export default Home
---
+++ src/components/nav.tsx
@@ -0,0 +1,12 @@
+function Nav() {
+
+    return (
+        <div className="nav-container">
+            <div className="nav-item">Nav Item</div>
+            <div className="nav-item">Nav Item</div>
+            <div className="nav-item">Nav Item</div>
+        </div>
+    )
+}
+
+export default Nav
